7b8f702c2906f58ee6f8bdc0c79e5550100993a8
F@nzil <nabiyarov.fanzil@gmail.com>
2021-11-24 18:51:41 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,7 +32,6 @@ sudo sed -i 's/PASS_MIN_DAYS.*/PASS_MIN_DAYS   2/' /etc/login.defs
 sudo sed -i 's/PASS_WARN_AGE.*/PASS_WARN_AGE   7/' /etc/login.defs
 sudo apt install libpam-pwquality -y
 sudo sed -i 's/.*pam_pwquality.so.*/password        requisite                       pam_pwquality.so retry=3 minlen=10 ucredit=-1 dcredit=-1 maxrepeat=3 reject_username difok=7 enforce_for_root/' /etc/pam.d/common-password
-sudo adduser fanzil
 sudo useradd -p $(perl -e 'print crypt($ARGV[0], "password")'fanzil) fanzil
 sudo chage -l fanzil
 sudo addgroup user42
