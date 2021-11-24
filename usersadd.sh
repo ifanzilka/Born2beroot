1aa0c953ec43ba63f016b0c0a5af1a498cc82768
F@nzil <nabiyarov.fanzil@gmail.com>
2021-11-24 18:54:54 +0300

    Update usersadd.sh

--- usersadd.sh
+++ usersadd.sh
@@ -1,3 +1,6 @@
+# echo "Ildus_2020" > tmp
+#sudo adduser ildus < tmp
+# rm tmp
 sudo useradd -p $(perl -e 'print crypt($ARGV[0], "password")'fanzil) fanzil
 sudo chage -l fanzil
 sudo addgroup user42
