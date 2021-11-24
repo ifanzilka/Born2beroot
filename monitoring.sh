dfe63d469760fc4ebc4f6d2063c9d826bc395ef8
F@nzil <nabiyarov.fanzil@gmail.com>
2021-11-24 19:35:44 +0300

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -4,7 +4,7 @@ $'\n#CPU physical: ' `cat /proc/cpuinfo | grep "physical id" | sort | uniq | wc 
 $'\n#vCPU: ' `nproc`\
 $'\n#Memory Usage: ' `free -m | grep Mem | awk ' {printf "%d/%dMB (%.2f%%)\n",$3,$2, $3*100/$2} '` \
 $'\n#Disk Usage: ' `df -k | grep root | awk '{ printf "%d",$3 / 1024} '``df -h | grep root | awk '{ printf "/%s (%s)\n",$2, $5} '`\
-$'\n#CPU Load: ' `cat /proc/loadavg  | awk '{ printf "%.1f%%" , $1}'`\
+$'\n#CPU Load: ' `top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}'`\
 $'\n#Last boot: ' `who -b | awk '{print $3 " " $4}'`\
 $'\n#LVM use: ' `lsblk | grep lvm  > /dev/null ; echo $? | awk ' {if ($1 == "0") print "yes"; else print "no"}'`\
 $'\n#Connection TCP: ' ` netstat -an | grep ESTABLISHED | wc -l` "ESTABLISHED"\
